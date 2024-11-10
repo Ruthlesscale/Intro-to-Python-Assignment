9c936dabd9a05efe11e715e29a9d93bd46619e0e
iavokeo <131613579+Ruthlesscale@users.noreply.github.com>
2024-11-10 21:09:01 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Get user input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+# Print a personalized message
+print(f"Hello, {name}! You are {age} years old and currently in {location}. Welcome!")
